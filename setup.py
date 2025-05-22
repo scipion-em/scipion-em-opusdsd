3d8665236029c6d55d71adb3a1712a254f2b8f0a
Eduardo <edugarde01@gmail.com>
2025-05-22 15:32:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
-
+"""
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
