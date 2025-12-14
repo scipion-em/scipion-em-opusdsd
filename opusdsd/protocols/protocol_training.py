23053b4a370cda06317f5226efbae2f0c312d96a
Eduardo Garcia <egarcia@cnb.csic.es>
2025-12-14 18:40:46 +0100

    Change in parameter section

--- opusdsd/protocols/protocol_training.py
+++ opusdsd/protocols/protocol_training.py
@@ -376,6 +376,9 @@ class OpusDsdProtTrain(ProtProcessParticles, ProtFlexBase):
         if self._getBoxSize() < 128:
             errors.append("OPUS-DSD requires a box size > 128 x 128 pixels.")
 
+        if self.templateres % 16 != 0:
+            errors.append("Template resolution must be even!")
+
         return errors
 
     # --------------------------- ARGS functions -----------------------------
