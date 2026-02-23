6296847587ccef268ec0156324eda47f8954ccb9
Eduardo Garcia <egarcia@cnb.csic.es>
2026-02-23 17:26:02 +0100

    Restriction of CPU threads in protocols when data overloading

--- opusdsd/protocols/protocol_training.py
+++ opusdsd/protocols/protocol_training.py
@@ -538,8 +538,17 @@ class OpusDsdProtTrain(ProtProcessParticles, ProtFlexBase):
 
     def _runProgram(self, program, args, fromXmipp=False):
         gpus = ','.join(str(i) for i in self.getGpuList())
+        threads = f'{self.numberOfThreads.get()}'
         if not fromXmipp:
-            self.runJob(Plugin.getProgram(program, gpus, fromCryodrgn=True), args, env=pwutils.Environ())
+            env = pwutils.Environ()
+            env.update({
+                'OMP_NUM_THREADS': threads,
+                'MKL_NUM_THREADS': threads,
+                'OPENBLAS_NUM_THREADS': threads,
+                'NUMEXPR_NUM_THREADS': threads,
+                'NUMBA_NUM_THREADS': threads
+            })
+            self.runJob(Plugin.getProgram(program, gpus, fromCryodrgn=True), args, env=env)
         else:
             self.runJob(Plugin.getXmippProgram(program), args)
 
