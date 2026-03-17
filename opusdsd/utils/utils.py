5ac0accfcf85a70115e29c2012c12ce19271bb9e
DavidHerreros <david.herreroscalero@gmail.com>
2026-03-17 13:48:59 +0100

    Fix paths for annotate space (relative instead of absolute)

--- opusdsd/utils/utils.py
+++ opusdsd/utils/utils.py
@@ -82,11 +82,11 @@ def getAnnotateSpaceArguments(particles, gpu_id=None):
     config = particles.getFlexInfo().getAttr(CONFIG)
     weights = particles.getFlexInfo().getAttr(WEIGHTS)
 
-    if f"projects + {os.path.sep}" in particles.getFlexInfo().getAttr(CONFIG):
-        config = particles.getFlexInfo().getAttr(CONFIG).split(f"projects + {os.path.sep}")[-1]
+    if f"projects{os.path.sep}" in particles.getFlexInfo().getAttr(CONFIG):
+        config = particles.getFlexInfo().getAttr(CONFIG).split(f"projects{os.path.sep}")[-1]
 
-    if f"projects + {os.path.sep}" in particles.getFlexInfo().getAttr(WEIGHTS):
-        weights = particles.getFlexInfo().getAttr(WEIGHTS).split(f"projects + {os.path.sep}")[-1]
+    if f"projects{os.path.sep}" in particles.getFlexInfo().getAttr(WEIGHTS):
+        weights = particles.getFlexInfo().getAttr(WEIGHTS).split(f"projects{os.path.sep}")[-1]
 
     args = (f"--config {config} --load {weights}"
             f" --server_functions_path {server_functions_path} --env_name {opusdsd.Plugin.getOpusDsdEnvActivation().split(' ')[-1]}")
